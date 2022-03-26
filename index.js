487cdb344dfb2764a6aee4874b9cb0bae8471a49
bernadette <aguirre.bernadette@outlook.com>
2022-03-25 21:05:30 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(plans = "roller-skate"){
+    return `This Saturday, I want to ${plans}!`
+}
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(programmer){
+   const innerFunction = function(overThinker = "special"){
+return `You are ${programmer}${overThinker}${programmer}!`;
+   }
+   return innerFunction
+}
